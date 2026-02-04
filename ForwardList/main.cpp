5d3d93eb33f39cf1f57ce47ff92052ec3d109991
. Gameplay <04astapov04@gmail.com>
2026-02-04 14:43:03 +0700

    Add project files.

---
+++ ForwardList/main.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n----------------------------\n"
+
+class Element
+{
+	int Data;
+	Element* pNext;
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
+	{
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend class ForwardList;
+};
+
+class ForwardList
+{
+	Element* Head;
+public:
+	ForwardList()
+	{
+		Head = nullptr;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~ForwardList()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+	void push_front(int Data)
+	{
+		Element* New = new Element(Data);
+		New->pNext = Head;
+		Head = New;
+	}
+	void push_back(int Data)
+	{
+		Element* New = new Element(Data);
+		Element* Temp = Head;
+		while (Temp->pNext) Temp = Temp->pNext;
+		Temp->pNext = New;
+	}
+	void print()const
+	{
+		Element* Temp = Head;
+		while (Temp)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			Temp = Temp->pNext;
+		}
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello ForwardList" << endl;
+	int n;
+	cout << "¬ведите размер списка: "; cin >> n;
+	ForwardList list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
+	list.push_back(123);
+	list.print();
+}
