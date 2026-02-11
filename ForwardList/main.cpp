c14ea6370f5a072991ace82385bae4d72f22e5ba
. Gameplay <04astapov04@gmail.com>
2026-02-11 13:36:34 +0700

    operator+ DONE

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -45,9 +45,24 @@ public:
 		while (size--)push_front(0);
 		cout << "1argConstructor:\t" << this << endl;
 	}
+	ForwardList(const ForwardList& other) :ForwardList()
+	{
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "LCopyConstructor:\t" << this << endl;
+	}
 	~ForwardList()
 	{
+		while (Head)pop_front();
 		cout << "LDestructor:\t" << this << endl;
+	}
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		return *this;
 	}
 	int& operator[](int Index)
 	{
@@ -55,11 +70,25 @@ public:
 		for (int i = 0; i < Index; i++)Temp = Temp->pNext;
 		return Temp->Data;
 	}
+	ForwardList operator+(const ForwardList& other) const
+	{
+		ForwardList result;  
+
+		for (Element* Temp = this->Head; Temp; Temp = Temp->pNext)
+			result.push_back(Temp->Data);
+
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			result.push_back(Temp->Data);
+
+		return result;
+	}
 	void push_front(int Data)
 	{
-		Element* New = new Element(Data);
+		/*Element* New = new Element(Data);
 		New->pNext = Head;
-		Head = New;
+		Head = New;*/
+		Head = new Element(Data, Head);
+
 		size++;
 	}
 	void push_back(int Data)
@@ -68,14 +97,14 @@ public:
 		Element* New = new Element(Data);
 		Element* Temp = Head;
 		while (Temp->pNext) Temp = Temp->pNext;
-		Temp->pNext = New;
+		Temp->pNext = new Element(Data);
 		size++;
 	}
 
 	void insert(int Data, int Index)
 	{
 		if (Index == 0)return push_front(Data);
-		Element* New = new Element(Data); 
+		//Element* New = new Element(Data); 
 
 		Element* Temp = Head;
 		for (int i = 0; i < Index - 1; i++)
@@ -83,8 +112,9 @@ public:
 			if (Temp->pNext == nullptr)break;
 			Temp = Temp->pNext;
 		}
-		New->pNext = Temp->pNext;
-		Temp->pNext = New;
+		//New->pNext = Temp->pNext;
+		//Temp->pNext = New;
+		Temp->pNext = new Element(Data, Temp->pNext);
 		size++;
 	}
 
@@ -142,12 +172,14 @@ public:
 
 	void print()const
 	{
-		Element* Temp = Head;
+		/*Element* Temp = Head;
 		while (Temp)
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;
-		}
+		}*/
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 		cout << "Количество элементов списка: " << size << endl;
 		cout << "Общее количество элементов : " << Element::count << endl;
 	}
@@ -155,8 +187,9 @@ public:
 
 //#define BASE_CHECK
 //#define SIZE_CHECK
-#define HOME_WORK_1
+//#define HOME_WORK_1
 //#define ERASE_CHECK
+//#define COPY_ASSIGNMENT_CHECK
 
 void main()
 {
@@ -227,4 +260,29 @@ void main()
 	list1.print();
 #endif // ERASE_CHECK
 
+#ifdef COPY_ASSIGNMENT_CHECK
+	ForwardList list1;
+	list1.push_back(3);
+	list1.push_back(5);
+	list1.push_back(8);
+	list1.push_back(13);
+	list1.push_back(21);
+	list1 = list1;
+	list1.print();
+
+	ForwardList list2;
+	list2 = list1;
+	list2.print();
+#endif // COPY_ASSIGNMENT_CHECK
+
+	ForwardList list1;
+	list1.push_back(1);
+	list1.push_back(2);
+	list1.push_back(3);
+	ForwardList list2;
+	list2.push_back(4);
+	list2.push_back(5);
+	list2.push_back(6);
+	ForwardList list3 = list1 + list2;
+	list3.print();
 }
