d0f39fbb7fe502b84e641b4360270b7d84cfa219
. Gameplay <04astapov04@gmail.com>
2026-02-25 16:36:33 +0700

    list2 base check DONE

--- List/main.cpp
+++ List/main.cpp
@@ -1,27 +1,31 @@
 #include<iostream>
 using namespace std;
 
-class Element
-{
-	int Data;
-	Element* pNext;
-	Element* pPrev;
-public:
-	Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
-	{
-		cout << "EConstructor" << endl;
-	}
-	~Element()
-	{
-		cout << "EDestructor" << endl;
-	}
-};
+#define tab "\t"
+#define delimiter "\n----------------------\n"
 
 class List
 {
-	int size;
-	Element* Head;
-	Element* Tail;
+
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr) :
+			Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor\t" << this << endl;
+		}
+		friend class List;
+	}*Head, * Tail;
+
+	size_t size;
 public:
 	int get_size()const
 	{
@@ -32,17 +36,144 @@ public:
 		size = 0;
 		Head = nullptr;
 		Tail = nullptr;
-		cout << "LConstructor" << endl;
+		cout << "LConstructor" << this << endl;
 	}
 	~List()
 	{
-		cout << "LDestructor" << endl;
+		while (Head)pop_front();
+		cout << "LDestructor" << this << endl;
+	}
+	void push_front(int Data) 
+	{
+		if (Head == nullptr && Tail == nullptr)
+			Head = Tail = new Element(Data);
+		else
+			Head = Head->pPrev = new Element(Data, Head);
+		size++;
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+			Head = Tail = new Element(Data);
+		else
+			Tail = Tail->pNext = new Element(Data, nullptr, Tail);
+		size++;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index > size)return;
+		if (Index == size - 1)return push_back(Data);
+		if (Index == 0)return push_front(Data);
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPrev;
+		}
+		Temp->pPrev->pNext = Temp->pNext->pPrev = new Element(Data, Temp->pNext, Temp->pPrev);
+		size++;
+	}
+	void erase(int index)
+	{
+		if (index >= size)return;
+		if (index == size - 1)return pop_back();
+		if (index == 0)return pop_front();
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
+		}
+		Temp->pPrev->pNext = Temp->pNext;
+		Temp->pNext->pPrev = Temp->pPrev;
+		delete Temp;
+		size--;
+	}
+	void print()const
+	{
+		cout << "Head:\t" << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Tail:\t" << Tail << endl;
+		cout << "Количество элементов в списке: " << size << endl;
+	}
+	void reverse_print()const
+	{
+		cout << "Tail:\t" << Tail << endl;
+		for (Element* Temp = Tail; Temp; Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Head:\t" << Tail << endl;
+	}
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Head = Head->pNext;
+			delete Head->pPrev;
+			Head->pPrev = nullptr;
+		}
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Tail;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Tail = Tail->pNext;
+			delete Head->pNext;
+			Tail->pNext = nullptr;
+		}
+		size--;
 	}
 };
 
+//#define BASE_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	List list1 = {};
+#ifdef BASE_CHECK
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
+	//list.reverse_print();
+
+	int index;
+	int value;
+	//cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	//cout << "Введите значение добавляемого элемента: "; cin >> value;
+	//list.insert(value, index);
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	list.erase(index);
+
+	list.print();
+#endif // BASE_CHECK
+
 }
